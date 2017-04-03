d8ee1e7b1e3ae923c93be87557b75e4c3bebb66a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-04-03 19:04:09 -0400

    load and not too many epochs

--- pixel_intensities.py
+++ pixel_intensities.py
@@ -8,14 +8,14 @@ import matplotlib.mlab as mlab
 from procedures.replay import reshape_to_row
 
 mnist = d.get('mnist')
-recns = np.load('data_optimized_notmedian.npy')[()]
+recns = np.load('data_optimized_notmedian_relumse.npy')[()]
 #  recns_notmedian = np.load('data_optimized_notmedian.npy')[()]
 
 og_means = []
 for clas in range(10):
     idx = np.where(np.where(mnist.og.train.labels == 1)[1] == clas)[0]
     og_means.append(np.mean(mnist.og.train.images[idx], axis=0))
-#  cv2.imshow('og means', reshape_to_row(np.array(og_means)))
+cv2.imshow('og means', reshape_to_row(np.array(og_means)))
 
 re_means = []
 for clas in range(10):
@@ -23,7 +23,7 @@ for clas in range(10):
     for batch_x, _ in recns[clas]:
         clas_imgs.extend(batch_x)
     re_means.append(np.mean(clas_imgs, axis=0))
-#  cv2.imshow('re means notmedian centralnorm', reshape_to_row(np.array(re_means)))
+cv2.imshow('re means notmedian centralnorm relumse', reshape_to_row(np.array(re_means)))
 
 #  cv2.waitKey(0)
 #  cv2.destroyAllWindows()
