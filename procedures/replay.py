6bfffbdd4f899338f19a6a2d155c1a2609fbcbc0
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-04-05 16:46:44 -0400

    revert, sfmt gave 17% rip

--- procedures/replay.py
+++ procedures/replay.py
@@ -228,16 +228,16 @@ def run(sess, f, data, placeholders, train_step, summary_op, summary_op_evaldist
                 sft = latent_placeholder
             else:
                 #  sft = tf.nn.sigmoid(latent_placeholder)
-                sft = tf.nn.softmax(latent_placeholder)
-                #  sft = tf.nn.relu(latent_placeholder)
+                #  sft = tf.nn.softmax(latent_placeholder)
+                sft = tf.nn.relu(latent_placeholder)
                 #  sft = latent_placeholder
             recreate_loss = tf.reduce_mean(
                     # MSE
                     #  tf.pow((sft - latent_recreated), 2))
                     # MSE on relu
-                    #  tf.pow((sft - tf.nn.relu(latent_recreated)), 2))
+                    tf.pow((sft - tf.nn.relu(latent_recreated)), 2))
                     # MSE on softmax
-                    tf.pow((sft - tf.nn.softmax(latent_recreated)), 2))
+                    #  tf.pow((sft - tf.nn.softmax(latent_recreated)), 2))
                     # SOFTMAX
                     #  tf.nn.softmax_cross_entropy_with_logits(
                         #  labels=sft, logits=latent_recreated, name='sftmax_xent'))
@@ -264,17 +264,17 @@ def run(sess, f, data, placeholders, train_step, summary_op, summary_op_evaldist
         # TODO: maybe this wrong
         temp_value = 8.0
         stats = compute_class_statistics(sess, '784-1200-1200-10/temp/div:0', inp, keep_inp, keep, data, 'temp_1:0', temp_value)
-        load_procedure = ['load', 'reconstruct_before', 'reconstruct_fly'][1]
+        load_procedure = ['load', 'reconstruct_before', 'reconstruct_fly'][0]
         if load_procedure == 'load':
             print('optimizing data')
-            data_optimized = np.load('data_optimized_notmedian_centralnorm_sftmmse.npy')[()]
+            data_optimized = np.load('data_optimized_notmedian_centralnorm_relumse.npy')[()]
         elif load_procedure == 'reconstruct_before':
             data_optimized = compute_optimized_examples(sess, stats,
                     f.train_batch_size, input_placeholder, latent_placeholder,
                     input_var, assign_op, recreate_op, data, latent_recreated,
                     recreate_loss, reinit_op, temp_recreated, temp_value)
 
-            np.save('data_optimized_notmedian_centralnorm_sftmmse.npy', data_optimized)
+            np.save('data_optimized_notmedian_centralnorm_relumse.npy', data_optimized)
 
         for i in range(f.epochs + 20):
             print('Epoch: {}'.format(i))
@@ -335,7 +335,7 @@ def run(sess, f, data, placeholders, train_step, summary_op, summary_op_evaldist
                 '784-800-800-10/temp/div:0', inp, keep_inp, keep, data, 'temp:0', temp_value)
         all_stats['teacher_stats'] = compute_class_statistics(sess,
                 '784-1200-1200-10/temp/div:0', inp, keep_inp, keep, data, 'temp_1:0', temp_value)
-        np.save('activation_stats_centralnorm_sftmmse.npy', all_stats)
+        np.save('activation_stats_centralnorm_relumse.npy', all_stats)
         print('stats_saved')
 
 
